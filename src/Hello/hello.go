bb8c8ea92529c59019281385ba3651071904f266
inherthz <ibanez_micro@hotmail.com>
2023-04-17 13:01:29 +0700

    3rd Commit

--- src/Hello/hello.go
+++ src/Hello/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println("Hello Patipol Gunhomepoo")
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
